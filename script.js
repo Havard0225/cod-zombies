52b6557b07373b76729f9ba787a81ecd6f003bb2
Havard0225 <scha012025@gmail.com>
2025-06-18 14:22:15 +0200

    .

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleNav() {
   navbar.classList.toggle("active");
 }
 
-// Lukk menyen hvis du klikker utenfor
+
 document.addEventListener("click", function(event) {
   if (!wrapper.contains(event.target)) {
     navbar.classList.remove("active");
